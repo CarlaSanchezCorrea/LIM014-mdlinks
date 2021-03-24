637165daf3b97bb5d45c5e7990e3c5aad6faff08
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-03-24 00:02:30 -0500

    add testing for index.js

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,28 @@
-const mdLinks = require('../');
+//const mdLinks = require('../');
+const index = require('../index.js');
 
 
-describe('mdLinks', () => {
+/*describe('mdLinks', () => {
 
   it('should...', () => {
     console.log('FIX ME!');
+  });
+
+});
+*/
+
+describe('index', () => {
+
+  it('Debería ser una funcion', () => {
+    expect(typeof index).toBe('function');
+  });
+
+});
+
+describe('realPath', () => {
+
+  it('Debería ser una funcion', () => {
+    expect(typeof realPath).toBe('function');
   });
 
 });
