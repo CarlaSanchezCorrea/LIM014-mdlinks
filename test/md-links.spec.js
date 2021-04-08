e0437c226e617827e5d99120f58e97d4d25e6040
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-04-08 11:25:21 -0500

    test typePsth & readFile

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,28 +1,20 @@
-//const mdLinks = require('../');
-const index = require('../index.js');
+const typePath = require('..\\src\\extract-links.js'); 
+const readFile = require('..\\src\\api.js'); 
+const relativePath = './test/prueba.md';
+const validate =[
+  {
+    href: 'https://es.wikipedia.org/wiki/Markdown',
+    text: 'Markdown',
+    file: 'test\\prueba.md'
+  }
+];
+const retornar = 'No es un archivo .md';
 
-
-/*describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('readFile', () => {
+  it('Debería ser una función (validatePath)', () => {
+    expect(typeof readFile).toBe('object');
   });
-
-});
-*/
-
-describe('index', () => {
-
-  it('Debería ser una funcion', () => {
-    expect(typeof index).toBe('function');
+  it('Debería retornar un array de cada link (href, text, file)', () => {
+    expect(readFile(relativePath)).toEqual(validate);
   });
-
-});
-
-describe('realPath', () => {
-
-  it('Debería ser una funcion', () => {
-    expect(typeof realPath).toBe('function');
-  });
-
 });
