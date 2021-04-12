3a553590295f6c6ad23ee4b06c96b6d428fb05ba
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-04-12 18:53:31 -0500

    change name md-links to api.spec.js

---
+++ test/api.spec.js
@@ -0,0 +1,20 @@
+//const typePath = require('..\\src\\extract-links.js'); 
+const {readFile} = require('../src/api.js'); 
+const relativePath = 'test\\prueba.md';
+const validate =[
+  {
+    href: 'https://es.wikipedia.org/wiki/Markdown',
+    text: 'Markdown',
+    file: 'test\\prueba.md'
+  }
+];
+//const retornar = 'No es un archivo .md';
+
+describe('readFile', () => {
+  it('Debería ser una object (readFile)', () => {
+    expect(typeof readFile).toBe('function');
+  });
+  it('Debería retornar un array de cada link (href, text, file)', () => {
+    expect(readFile(relativePath)).toEqual(validate);
+  });
+});
